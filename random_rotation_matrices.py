1f4f3d980cd9d18ae8f5b6a6a4e729da5b5482b2
Ratnadeep Bhattacharya <ratnadeepb@users.noreply.github.com>
2017-05-14 17:12:42 +0400

    Added a 3D plot function.

--- random_rotation_matrices.py
+++ random_rotation_matrices.py
@@ -39,3 +39,40 @@ def random_rotation():
     This function creates a random rotation matrix
     """
     return orthogonalization(1 * np.random.randn(9).reshape(3,3))
+
+def plot_rotation(u, v):
+    """
+    This function will plot u and v vectors in 3D
+    """
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    xx, yx, zx = [1, 0], [0, 0], [0, 0]
+    xy, yy, zy = [0, 0], [0, 1], [0, 0]
+    xz, yz, zz = [0, 0], [0, 0], [0, 1]
+    x0, y0, z0 = [0, u[0]], [0, u[1]], [0, u[2]]
+    x1, y1, z1 = [0, v[0]], [0, v[1]], [0, v[2]]
+    ax.plot(xx, yx, zx, 'r')
+    ax.text(1, 0, 0, 'x')
+    ax.plot(xy, yy, zy, 'g')
+    ax.text(0, 1, 0, 'y')
+    ax.plot(xz, yz, zz, 'b')
+    ax.text(0, 0, 1, 'z')
+    ax.plot(x0, y0, z0, 'k', linestyle=':')
+    ax.text(u[0], u[1], u[2], '({:.3f},{:.3f},{:.3f})'.format(u[0], u[1], u[2]))
+    ax.plot(x1, y1, z1, 'k', linestyle='--')
+    ax.text(v[0], v[1], v[2], '({:.3f},{:.3f},{:.3f})'.format(v[0], v[1], v[2]))
+    plt.show()
+
+def rotate():
+    """
+    This function will choose a u vector in random
+    Generate the R matrix
+    Rotate u by R to v
+    Plot v
+    Return v
+    """
+    u = np.random.rand(3)
+    R = random_rotation()
+    v = R @ u
+    plot_rotation(u, v)
+    return v
